359fdad73ced1da2d7d2d9bc1b96ac2e64d7e0db
Bohdan Tkachenko <bohdan@doc.ai>
2018-03-27 12:57:34 -0700

    ci(Travis): Add cluster project env var

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,20 +2,21 @@
 
 set -e
 
-if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 <service account filename> <project> <cluster name> <cluster zone> <tag>"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 <service account filename> <project> <cluster project> <cluster name> <cluster zone> <tag>"
     exit 1
 fi
 
 SERVICE_ACCOUNT_FILENAME=$1
 PROJECT=$2
-CLUSTER_NAME=$3
-CLUSTER_ZONE=$4
-TAG=$5
+CLUSTER_PROJECT=$3
+CLUSTER_NAME=$4
+CLUSTER_ZONE=$5
+TAG=$6
 
 docker push docai/$PROJECT
 gcloud auth activate-service-account --key-file=$SERVICE_ACCOUNT_FILENAME
 gcloud container clusters get-credentials $CLUSTER_NAME \
   --zone $CLUSTER_ZONE \
-  --project CLUSTER_PROJECT
+  --project $CLUSTER_PROJECT
 kubectl set image deployment/$PROJECT $PROJECT=docai/$PROJECT:$TAG
